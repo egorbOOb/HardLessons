732b5092631e1950eeb6852cb2d15a3ea22c40cb
egrbOObs <egor01.6@inbox.ru>
2020-06-10 03:13:07 +0700

    Мой супер-вариант с проверкой на русские символы

---
+++ script.js
@@ -0,0 +1,37 @@
+let lang = prompt('Укажите язык: en или ru');
+
+let weekDaysEn = ['sunday', ' monday', ' tuesday', ' wednesday', ' thursday', ' friday' , ' saturday'];
+let weekDaysRu = ['понедельник', ' вторник', ' среда', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'];
+
+
+
+//a.
+if (lang === 'en') {
+    console.log(weekDaysEn);
+} else if (lang === 'ru') {
+    console.log(weekDaysRu);
+};
+
+
+
+//b.
+switch (lang) {
+    case 'en':
+        alert(weekDaysEn);
+        console.log(weekDaysEn);
+        break;
+    case 'ru':
+        alert(weekDaysRu)
+        console.log(weekDaysRu);
+        break;
+};
+
+
+
+//c.
+let  diffLanguageOption = [
+    ['sunday', ' monday', ' tuesday', ' wednesday', ' thursday', ' friday' , ' saturday'],
+    ['понедельник', ' вторник', ' среда', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'],
+];
+
+lang === 'en' ? console.log(diffLanguageOption[0]) : console.log(diffLanguageOption[1]);
