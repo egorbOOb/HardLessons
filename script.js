4f48d1b2bb076b4a7f137bb8b869dd0fa07ce7f8
egrbOObs <egor01.6@inbox.ru>
2020-06-10 03:54:05 +0700

    Усложнённое задание к 3-ему уроку

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1.
 let lang = prompt('Укажите язык: en или ru');
 
 let weekDaysEn = ['sunday', ' monday', ' tuesday', ' wednesday', ' thursday', ' friday' , ' saturday'];
@@ -35,3 +36,12 @@ let  diffLanguageOption = [
 ];
 
 lang === 'en' ? console.log(diffLanguageOption[0]) : console.log(diffLanguageOption[1]);
+
+
+
+//2.
+let namePerson = prompt('Введите имя');
+
+(namePerson === 'Артем') ? console.log('Директор') :
+    ( namePerson === 'Максим') ? console.log('Преподаватель') :
+    console.log('Студент');
