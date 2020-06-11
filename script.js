094dd564e362f002ec3aaf12b704d15549e568e1
egrbOObs <egor01.6@inbox.ru>
2020-06-11 17:23:53 +0700

    Услодненное задание к уроку №4

--- script.js
+++ script.js
@@ -45,3 +45,20 @@ let namePerson = prompt('Введите имя');
 (namePerson === 'Артем') ? console.log('Директор') :
     ( namePerson === 'Максим') ? console.log('Преподаватель') :
     console.log('Студент');
+
+
+
+    //Урок №4
+
+function getCleanStr(str) {
+    let size = 30;
+    if (typeof(str) !== 'string') {
+        return 'Введенное значение должно быть строкой';
+    } else if (str.trim().length >= size) {
+        return str.trim().slice(0, size) + '...';
+    } else if (str.trim().length < size) {
+        return str.trim();
+    } 
+};
+
+console.log(getCleanStr('\t 55    344535533553355353535353535353353   '));
