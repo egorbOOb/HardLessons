b9bcb7fce71eedef8f77418081aa7fccf1b9306d
egrbOObs <egor01.6@inbox.ru>
2020-06-13 05:42:41 +0700

    Усложненное задание к уроку №5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1.
-/*let lang = prompt('Укажите язык: en или ru');
+let lang = prompt('Укажите язык: en или ru');
 
 let weekDaysEn = ['sunday', ' monday', ' tuesday', ' wednesday', ' thursday', ' friday' , ' saturday'];
 let weekDaysRu = ['понедельник', ' вторник', ' среда', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'];
@@ -46,9 +46,9 @@ let namePerson = prompt('Введите имя');
     ( namePerson === 'Максим') ? console.log('Преподаватель') :
     console.log('Студент');
 
-*/
 
-    //Урок №4
+
+//Урок №4
 function getStr() {
     let str;
     do {
@@ -71,3 +71,44 @@ function showCleanStr() {
 
 showCleanStr();
 
+//Урок №5
+
+let arr = ['23451', '46464', '5552', '3142', '4678', '9699', '851046'];
+
+
+let numberss;
+
+function showDesiredNumbers() {
+
+    for (let i = 0; i < arr.length; i++) {
+
+        numberss = arr[i];
+
+        if (Number(numberss[0]) === 2 || Number(numberss[0]) === 4) {
+
+            console.log(arr[i]);
+        
+        };
+    }
+};
+
+    
+showDesiredNumbers()
+
+
+
+
+let numbers = [];
+
+
+Next: for (let i = 2; i < 10; i++) {
+    for (let y = 2; y < i; y++) {
+        if (i % y === 0) {
+            continue Next;
+        }
+
+    }
+
+    numbers.push(i);
+    console.log(i + '- Делители этого числа: 1 и ' + i);
+}
